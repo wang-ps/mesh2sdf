4f989a4688e3e6e0af0777ccaaed9c8240172bf6
wang-ps <wangps@hotmail.com>
2021-08-30 14:29:06 +0800

    Test python scripts

--- sdf2npy.py
+++ sdf2npy.py
@@ -11,9 +11,11 @@ parser.add_argument('--root_folder', required=True, type=str)
 parser.add_argument('--filelist', required=True, type=str)
 parser.add_argument('--output_path', required=True, type=str)
 parser.add_argument('--scale', type=float, default=128)
-parser.add_argument('--level', type=float, default=0.05)  # 2/128 = 0.015625
+parser.add_argument('--level', type=float, default=0.015)  # 2/128 = 0.015625
+parser.add_argument('--start', type=int, default=0)
+parser.add_argument('--end', type=int, default=57449)
 parser.add_argument('--sdfgen', type=str,
-                    default='build/bin/Release/SDFGen.exe')
+                    default='build\\bin\\Release\\SDFGen.exe')
 parser.add_argument('--pieces', type=str,
                     default='extract_pieces')
 args = parser.parse_args()
@@ -21,54 +23,61 @@ mul = 0.8
 
 with open(args.filelist, 'r') as fid:
   lines = fid.readlines()
-filenames = [line.strip() for line in lines]
+filenames = [line.split()[0] for line in lines]
 
-for filename in tqdm(filenames[4660:], ncols=80):
-  filename_in = os.path.join(args.root_folder, filename + '.obj')
-  filename_obj = os.path.join(args.output_path + '.obj', filename + '.obj')
-  filename_nobj = os.path.join(args.output_path + '.new.obj', filename + '.obj')
+for filename in tqdm(filenames[args.start:args.end], ncols=80):
+  filename_in = os.path.join(args.root_folder, filename)
+  filename_sobj = os.path.join(args.output_path + '.scale', filename)
+  filename_tobj = os.path.join(args.output_path + '.raw', filename)
+  filename_nobj = os.path.join(args.output_path + '.new', filename)
+
+  path_obj = os.path.dirname(filename_sobj)
+  if not os.path.exists(path_obj):
+    os.makedirs(path_obj)
+  path_tmp = os.path.dirname(filename_tobj)
+  if not os.path.exists(path_tmp):
+    os.makedirs(path_tmp)
 
   # load mesh and rescale mesh
-  mesh = trimesh.load_mesh(filename_in)
+  mesh = trimesh.load(filename_in, force='mesh')
   bbmin, bbmax = mesh.vertices.min(axis=0), mesh.vertices.max(axis=0)
   center = (bbmin + bbmax) / 2.0
   scale = 2.0 * mul / ((bbmax - bbmin).max() + 1.0e-6)
-  mesh.vertices = (mesh.vertices - center)
-  mesh.export(filename_obj)
+  mesh = trimesh.Trimesh((mesh.vertices - center) * scale, mesh.faces)
+  mesh.export(filename_sobj)
 
   # run sdfgen
-  cmds = [args.sdfgen, filename_obj, str(2.0 / args.scale)]
+  cmds = [args.sdfgen, filename_sobj, str(2.0 / args.scale)]
   cmd = ' '.join(cmds)
   os.system(cmd)
 
   # convert sdfgen to npy
-  filename_npy = filename_obj[:-4] + '.npy'
-  filename_sdf = filename_obj[:-4] + '.sdf'
+  # filename_npy = filename_obj[:-4] + '.npy'
+  filename_sdf = filename_sobj[:-4] + '.sdf'
   sdf = np.fromfile(filename_sdf, dtype=np.float32)
   sdf = np.reshape(sdf, [args.scale] * 3)
   sdf = np.transpose(sdf, (2, 1, 0))
-  np.save(filename_npy, sdf)
+  # np.save(filename_npy, sdf)
+  os.remove(filename_sdf)
 
   # extract mesh
-  filename_tobj = filename_obj[:-4] + '.tmp.obj'
   vtx, faces, _, _ = skimage.measure.marching_cubes_lewiner(sdf, args.level)
+  vtx = vtx * (2.0 / args.scale) - 1.0
   mesh = trimesh.Trimesh(vtx, faces)
   mesh.export(filename_tobj)
 
-  # keep the max component of the extracted mesh
-  # cmds = [args.pieces, '--filenames', filename_tobj, '']
+# keep the max component of the extracted mesh
+# cmds = [args.pieces, '--filenames', filename_tobj, '']
 
+# # run sdfgen again
+# cmds = [args.sdfgen, filename_nobj, str(2.0 / args.scale)]
+# cmd = ' '.join(cmds)
+# os.system(cmd)
 
-
-  # run sdfgen again
-  cmds = [args.sdfgen, filename_nobj, str(2.0 / args.scale)]
-  cmd = ' '.join(cmds)
-  os.system(cmd)
-
-  # convert sdfgen to npy
-  filename_npy = filename_nobj[:-4] + '.npy'
-  filename_sdf = filename_nobj[:-4] + '.sdf'
-  sdf = np.fromfile(filename_sdf, dtype=np.float32)
-  sdf = np.reshape(sdf, [args.scale] * 3)
-  sdf = np.transpose(sdf, (2, 1, 0))
-  np.save(filename_npy, sdf)
+# # convert sdfgen to npy
+# filename_npy = filename_nobj[:-4] + '.npy'
+# filename_sdf = filename_nobj[:-4] + '.sdf'
+# sdf = np.fromfile(filename_sdf, dtype=np.float32)
+# sdf = np.reshape(sdf, [args.scale] * 3)
+# sdf = np.transpose(sdf, (2, 1, 0))
+# np.save(filename_npy, sdf)
--- visualize_sdf.py
+++ visualize_sdf.py
@@ -1,11 +1,12 @@
+import sys
 import matplotlib.pyplot as plt
-
 import numpy as np
 import skimage.measure
 import trimesh
 
+filename = sys.argv[1]
+# filename = 'C:/Users/penwan/Desktop/debug/plane.npy'
 
-filename = 'C:/Users/penwan/Desktop/debug/bunny.sdf.npy'
 # levels = [-0.2, -0.1, -0.05, -0.02, -0.01, 0.0, 0.01, 0.02, 0.05, 0.1, 0.2]
 levels = [-0.01, 0.0, 0.01]
 
@@ -23,7 +24,6 @@ for i, level in enumerate(levels):
   mesh = trimesh.Trimesh(vtx, faces)
   mesh.export(filename[:-3] + 'l%.2f.obj' % level)
 
-
   # mesh = o3d.geometry.TriangleMesh()
   # mesh.vertices = o3d.utility.Vector3dVector(vtx)
   # mesh.triangles = o3d.utility.Vector3iVector(faces)
@@ -32,21 +32,22 @@ for i, level in enumerate(levels):
 
 
 ## draw image
-array_2d = sdf[64,:,:]
+for i in range(128):
+  array_2d = sdf[:,:,i]
 
-num_levels = 6
-fig, ax = plt.subplots(figsize=(2.75, 2.75), dpi=300)
-levels_pos = np.logspace(-2, 0, num=num_levels)  # logspace
-levels_neg = -1. * levels_pos[::-1]
-levels = np.concatenate((levels_neg, np.zeros((0)), levels_pos), axis=0)
-colors = plt.get_cmap("Spectral")(np.linspace(0., 1., num=num_levels*2+1))
+  num_levels = 6
+  fig, ax = plt.subplots(figsize=(2.75, 2.75), dpi=300)
+  levels_pos = np.logspace(-2, 0, num=num_levels)  # logspace
+  levels_neg = -1. * levels_pos[::-1]
+  levels = np.concatenate((levels_neg, np.zeros((0)), levels_pos), axis=0)
+  colors = plt.get_cmap("Spectral")(np.linspace(0., 1., num=num_levels*2+1))
 
-sample = np.flipud(array_2d)
-CS = ax.contourf(sample, levels=levels, colors=colors)
-# cbar = fig.colorbar(CS)
+  sample = np.flipud(array_2d)
+  CS = ax.contourf(sample, levels=levels, colors=colors)
+  # cbar = fig.colorbar(CS)
 
-ax.contour(sample, levels=levels, colors='k', linewidths=0.1)
-ax.contour(sample, levels=[0], colors='k', linewidths=0.3)
-ax.axis('off')
-# plt.savefig(filename + '.png')
-plt.show()
+  ax.contour(sample, levels=levels, colors='k', linewidths=0.1)
+  ax.contour(sample, levels=[0], colors='k', linewidths=0.3)
+  ax.axis('off')
+  plt.savefig(filename + '.z.%03d.png' % i)
+  # plt.show()
